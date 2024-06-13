b42985c60f48a73e58214fb8321f2fc3fd8e6522
GabsRMartins <gabsmartins262@gmail.com>
2024-06-13 10:12:09 -0300

    feat:Base Interface

--- src/principal/main.cpp
+++ src/principal/main.cpp
@@ -9,19 +9,33 @@
 #include "../../include/inimigo-hpp/Zumbi.hpp"
 
 
-#include <iostream>
+#include "iostream"
 #include <thread>
 #include <chrono>
-#include <memory> // Para std::make_unique
-// Aqui temos duas bibliotecas importantes:
-// - <thread>: Permite criar e gerenciar threads (processos paralelos).
-// - <chrono>: Fornece funções para medir o tempo e criar pausas.
+#include <memory> 
 
+using std::cout;
 using std::make_unique;
+using std::endl;
 
 
-int main() {
-    Simulador simulador(10,10); // Definindo o tamanho do mapa como 10 posições
+
+void mapaPadrao(Simulador& simulador){
+    MapaPadrao mapa(10,10);
+    mapa.MontaMapa(simulador);
+
+    simulador.adicionarTorre(make_unique<TorreDeFogo>(5,1));
+    simulador.adicionarTorre(make_unique<TorreDeFogo>(6,1));
+    simulador.adicionarTorre(make_unique<TorreDeFogo>(0,1));
+
+    simulador.adicionarInimigo(make_unique<Esqueleto>(0,0));
+    simulador.adicionarInimigo(make_unique<Esqueleto>(2,0)); 
+    simulador.adicionarInimigo(make_unique<Esqueleto>(4,2)); 
+    simulador.adicionarInimigo(make_unique<Esqueleto>(2,2));  
+};
+
+void cemiterio(Simulador& simulador){
+
     Cemiterio mapa(10,10);
     mapa.MontaMapa(simulador);
 
@@ -29,14 +43,37 @@ int main() {
     simulador.adicionarTorre(make_unique<TorreDeFogo>(6,1));
     simulador.adicionarTorre(make_unique<TorreDeFogo>(0,1));
 
-
-    // Adiciona inimigos no mapa
     simulador.adicionarInimigo(make_unique<Zumbi>(0,0));
     simulador.adicionarInimigo(make_unique<Zumbi>(2,0)); 
     simulador.adicionarInimigo(make_unique<Zumbi>(4,2)); 
     simulador.adicionarInimigo(make_unique<Zumbi>(2,2));  
 
 
+};
+
+
+
+int main() {
+    Simulador simulador(10,10); 
+
+    cout<<"Bem vindo ao Defense PDS 2"<< endl;
+    int opcao;
+    cout<<"Escolha o Mapa";
+    cout<<" [1] Mapa Padrão || [2] Cemitério"<< endl;
+    scanf("%d",&opcao);
+    switch (opcao)
+    {
+    case 1:
+        mapaPadrao(simulador);
+       break;
+
+   case 2:     
+             cemiterio(simulador);
+    break;
+    default:
+            cout << "Opção inválida!" << endl;
+            return 1;
+    };
     // Inicia a simulação
     simulador.simular();
     return 0;
